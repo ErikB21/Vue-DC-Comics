6f417aa82b3e0898deeba3544305dd27d8c0007a
ErikBorgogno <erikborgo.20@gmail.com>
2022-08-24 16:26:49 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderComics/>
+    <MainComics/>
+    <FooterComics/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MainComics from './components/MainComics.vue';
+import HeaderComics from './components/HeaderComics.vue';
+import FooterComics from './components/FooterComics.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainComics,
+    HeaderComics,
+    FooterComics
+}
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MainComics.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'MainComics'
+}
+</script>
+
+<style>
+
+</style>
