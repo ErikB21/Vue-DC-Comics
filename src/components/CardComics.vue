9e1b95e1b9c04d0f5576c5af306a7d1a80516822
ErikBorgogno <erikborgo.20@gmail.com>
2022-08-25 16:38:20 +0200

    project done

---
+++ src/components/CardComics.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card border border-0 pt-5">
+        <img :src="imageSrc" :alt="title">
+        <h3 class="h5 text-white text-center py-1">{{title}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComics',
+    props: {
+        imageSrc: String,
+        title: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import '../assets/style/vars.scss';
+    .card{
+      width: calc(100% / 6 - 100px);
+      height: calc(100% / 2);
+      margin: 10px 50px;
+      background-color: $bg_main;
+      img{
+        width: 100%;
+      }
+    }
+</style>
